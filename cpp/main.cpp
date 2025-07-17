43a84fd830c7e5af079e923862a0448e7eec1459
mirjunaid26 <mir.jnd@gmail.com>
2025-07-17 14:09:23 +0200

    add swap

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 
 int main() {
-    std::cout << "Hello World!";
+    int a = 1;
+    int b = 2;
 
+    std::cout << a << b << std::endl;
+
+    // let us swap values
+    int temp = a;
+    a = b;
+    b = temp;
+    
+    std::cout << a << b;
+    
     return 0;
+
 }
