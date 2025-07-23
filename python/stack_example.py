bedad8ab58bfd5a3eaf5873211c82b27a88dfe97
mirjunaid26 <mir.jnd@gmail.com>
2025-07-23 17:07:38 +0200

    modify python stack

--- python/stack_example.py
+++ python/stack_example.py
@@ -2,7 +2,6 @@ stack = []
 
 for i in range(1, 8):
     stack.append(i)
-    
     print("Pushed: ", i)
     
-print("\nStack size after pushing: ", len(stack))
+print("Stack size after pushing: ", len(stack))
