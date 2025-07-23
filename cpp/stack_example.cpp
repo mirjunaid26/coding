83fb912aa8f642c9f0126d8eba1c88aff098755a
mirjunaid26 <mir.jnd@gmail.com>
2025-07-23 17:19:16 +0200

    pop elemts from stack

--- cpp/stack_example.cpp
+++ cpp/stack_example.cpp
@@ -2,15 +2,23 @@
 #include <stack>
 
 int main() {
-    std::stack<int> stack;
+    std::stack<int> s;
 
+    // Push 7 elemets to the stack
     for (int i = 1; i <= 7; i++) {
-        stack.push(i);
+        s.push(i);
         std::cout << "Pushed: " << i << std::endl;
             }
 
-    std::cout << "Stack after pushing element: " << stack.size() << std::endl;
+    std::cout << "Stack after pushing element: " << s.size() << std::endl;
     
+    // Pop and display all elements
+    std::cout << "\nPopping elements:\n";
+    while (!s.empty()) {
+        std::cout << "Popped: " << s.top() << std::endl;
+        s.pop();
+    }
+
     return 0;
 
 }
