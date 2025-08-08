d84302320239c90b7e34ae01d12f1bb1b1c80783
mirjunaid26 <mir.jnd@gmail.com>
2025-08-08 08:36:13 +0200

    stage changes

--- pytorch-tutorial/tensors.py
+++ pytorch-tutorial/tensors.py
@@ -54,3 +54,49 @@ print("----------")
 
 flat_tensor2 = np_array.reshape(-1) # flatten the numpy array
 print(flat_tensor2)
+
+# Attributes of a tensor
+tensor = torch.rand(3, 4)
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Tensor is stored on: {tensor.device}")
+print("----------")
+
+# Operations on tensors
+if torch.cuda.is_available():
+    tensor = tensor.to(torch.cuda.current_device())
+    print(f"Tensor is now stored on: {tensor.device}")
+else:
+    print("CUDA is not available, tensor remains on CPU.")
+    
+# Standard numpy-like indexing and slicing
+tensor = torch.ones(4, 4)
+print(f"First row: {tensor[0]}")
+print(f"First Column: {tensor[:, 0]}")
+print(f"Last column: {tensor[..., -1]}")
+tensor[:, 1] = 0 # change the second column to zero
+tensor[:, 2] = 0 # change the third column to zero
+print("Modified tensor:", tensor)
+#print(tensor)
+print("----------")
+
+# Joining tensors
+t1 = torch.cat([tensor], dim=1)
+print(t1)
+print("----------")
+t2 = t1.view(-1)
+print(t2)
+print("----------")
+t3 = t2.view(4, 4)
+print(t3)
+
+t4 = torch.rand(8, 8)
+print(t4)
+t5 = t4.view(-1)
+print(t5)
+
+t6 = torch.cat([t2, t5], dim=0)
+print(t6)
+t6_shape = t6.shape
+print("Shape of t6:", t6_shape)
+
